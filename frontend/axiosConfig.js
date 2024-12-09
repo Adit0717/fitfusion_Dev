4efdc0bce7dd9047b2b503042e030ff2cd7c9074
Harsha <kunahk01@pfw.edu>
2024-12-08 20:21:53 -0500

    axios changes

--- frontend/axiosConfig.js
+++ frontend/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080', // Base URL for all requests
+  baseURL: 'https://fit-fusion-app-f7f9c3528528.herokuapp.com', // Base URL for all requests
   timeout: 10000, // Optional: Set a timeout for requests (in milliseconds)
   headers: {
     'Content-Type': 'application/json', // Default headers
